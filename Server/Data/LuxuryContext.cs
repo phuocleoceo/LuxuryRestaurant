69aada3e264c9e0f6e7ce9d493d784bc5c9312de
phuocleoceo <ht10082001@gmail.com>
2021-11-05 22:57:07 +0700

    DB Online

--- Server/Data/LuxuryContext.cs
+++ Server/Data/LuxuryContext.cs
@@ -6,8 +6,14 @@ namespace Server.Data
 {
     public class LuxuryContext : DbContext
     {
-        private readonly string CNS = @"Server=(LocalDB)\MSSQLLocalDB;Database=LuxuryRestaurant;
-                            Trusted_Connection=True;MultipleActiveResultSets=true";
+        //private readonly string CNS = @"Server=(LocalDB)\MSSQLLocalDB;Database=LuxuryRestaurant;
+        //                    Trusted_Connection=True;MultipleActiveResultSets=true";
+        private readonly string CNS = @"workstation id=LuxuryRestaurant.mssql.somee.com;
+                                        packet size=4096;user id=phuocleoceo_SQLLogin_1;
+                                        pwd=8q9d4uns68;
+                                        data source=LuxuryRestaurant.mssql.somee.com;
+                                        persist security info=False;
+                                        initial catalog=LuxuryRestaurant";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -26,6 +32,6 @@ namespace Server.Data
         public DbSet<User> Users { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
-        public DbSet<OrderHeader> OrderHeaders { get; set; }        
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
     }
 }
