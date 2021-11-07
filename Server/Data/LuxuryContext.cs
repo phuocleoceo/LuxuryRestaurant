b2dea63ed585a1752d44ec2a99510957279350f8
phuocleoceo <ht10082001@gmail.com>
2021-11-08 00:11:45 +0700

    FormManage

--- Server/Data/LuxuryContext.cs
+++ Server/Data/LuxuryContext.cs
@@ -6,14 +6,14 @@ namespace Server.Data
 {
     public class LuxuryContext : DbContext
     {
-        //private readonly string CNS = @"Server=(LocalDB)\MSSQLLocalDB;Database=LuxuryRestaurant;
-        //                    Trusted_Connection=True;MultipleActiveResultSets=true";
-        private readonly string CNS = @"workstation id=LuxuryRestaurant.mssql.somee.com;
-                                        packet size=4096;user id=phuocleoceo_SQLLogin_1;
-                                        pwd=8q9d4uns68;
-                                        data source=LuxuryRestaurant.mssql.somee.com;
-                                        persist security info=False;
-                                        initial catalog=LuxuryRestaurant";
+        private readonly string CNS = @"Server=(LocalDB)\MSSQLLocalDB;Database=LuxuryRestaurant;
+                            Trusted_Connection=True;MultipleActiveResultSets=true";
+        //private readonly string CNS = @"workstation id=LuxuryRestaurant.mssql.somee.com;
+        //                                packet size=4096;user id=phuocleoceo_SQLLogin_1;
+        //                                pwd=8q9d4uns68;
+        //                                data source=LuxuryRestaurant.mssql.somee.com;
+        //                                persist security info=False;
+        //                                initial catalog=LuxuryRestaurant";
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
