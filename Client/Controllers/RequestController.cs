6e89d6a268c6f78ce5dd607a88623219df80e672
phuocleoceo <ht10082001@gmail.com>
2021-11-20 19:48:59 +0700

    Request Index

---
+++ Client/Controllers/RequestController.cs
@@ -0,0 +1,27 @@
+﻿using Client.Repository.Interface;
+using Common.DTO;
+using Common.Entities;
+using Common.Extension;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    [Authorize(Roles = Constant.Role_Customer)]
+    public class RequestController : Controller
+    {
+        public RequestController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
